55c1789d94bf9f55094956ed1110381561b0d672
qiang <jhq0113@163.com>
2018-12-30 17:38:42 +0800

    Hello world

---
+++ demos/hello/application/controllers/Index.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jiang Haiqiang
+ * Date: 2018/12/30
+ * Time: 5:35 PM
+ */
+
+class IndexController extends Yaf_Controller_Abstract
+{
+    // default action name
+    public function indexAction()
+    {
+        $this->getView()->content = "Hello World";
+    }
+}
---
+++ demos/hello/public/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jiang Haiqiang
+ * Date: 2018/12/30
+ * Time: 5:32 PM
+ */
+
+define("APPLICATION_PATH",  dirname(dirname(__FILE__)));
+
+$app  = new Yaf_Application(APPLICATION_PATH . "/conf/application.ini");
+$app->run();
