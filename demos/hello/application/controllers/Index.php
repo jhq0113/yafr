1ccdb5293fcf216140393c22e6dd60f2def25c5c
qiang <jhq0113@163.com>
2018-12-31 12:32:35 +0800

    环境安装

--- demos/hello/application/controllers/Index.php
+++ demos/hello/application/controllers/Index.php
@@ -5,22 +5,15 @@
  * Date: 2018/12/30
  * Time: 5:35 PM
  */
-
-class IndexController extends Yaf\Controller_Abstract
+class IndexController extends Yaf_Controller_Abstract
 {
     // default action name
     public function indexAction()
     {
-        //$this->getView()->content = "Hello World";
-        $cons = get_defined_constants();
-        var_dump($cons);die;
+        $object = new \models\BaseModel();
+        $object->name = 'sdfasfd';
+        echo $object->name;die;
 
-        $config = \Yaf\Application::app()->getConfig();
-
-        var_dump($config);die;
-
-        $object = new \com\BaseObject("Hello World!");
-
-        $this->getView()->content = $object->name;
+        $this->getView()->content = "Hello World";
     }
 }
---
+++ demos/hello/application/library/models/BaseModel.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jiang Haiqiang
+ * Date: 2018/12/31
+ * Time: 12:31 PM
+ */
+
+namespace models;
+
+
+class BaseModel
+{
+    public $name;
+
+
+}
