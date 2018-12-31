cef202a0223cada95de3adfa573b28673658dde5
qiang <jhq0113@163.com>
2018-12-31 13:39:43 +0800

    1.将所有yaf类变为命名空间方式

--- demos/hello/public/index.php
+++ demos/hello/public/index.php
@@ -8,5 +8,5 @@
 
 define("APPLICATION_PATH",  dirname(dirname(__FILE__)));
 
-$app  = new Yaf_Application(APPLICATION_PATH . "/conf/application.ini");
+$app  = new Yaf\Application(APPLICATION_PATH . "/conf/application.ini");
 $app->run();
