acf3311f1b47976622a9c46e3b8b77479128e82c
qiang <jhq0113@163.com>
2018-12-31 12:49:50 +0800

    环境安装

--- demos/hello/application/controllers/Index.php
+++ demos/hello/application/controllers/Index.php
@@ -5,21 +5,11 @@
  * Date: 2018/12/30
  * Time: 5:35 PM
  */
-class IndexController extends Yaf\Controller_Abstract
+class IndexController extends Yaf_Controller_Abstract
 {
     // default action name
     public function indexAction()
     {
-        try {
-            $object = new services\BaseServices();
-            $object->name = 'sdfasfd';
-            echo $object->name;die;
-        }catch (\Exception $exception) {
-            var_dump($exception->getMessage());
-            die;
-        }
-
-
         $this->getView()->content = "Hello World";
     }
 }
