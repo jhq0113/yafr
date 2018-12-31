860382e29b8968476afb3e8c647ced021698d262
qiang <jhq0113@163.com>
2018-12-31 20:47:53 +0800

    1.添加bootstrap

---
+++ demos/hello/application/Bootstrap.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jiang Haiqiang
+ * Date: 2018/12/31
+ * Time: 8:44 PM
+ */
+
+class Bootstrap extends \Yaf\Bootstrap_Abstract
+{
+    /**
+     * @author Jiang Haiqiang
+     * @email  jhq0113@163.com
+     */
+    public function _initDb()
+    {
+        echo __FUNCTION__.'<br/>';
+    }
+
+    /**
+     * @author Jiang Haiqiang
+     * @email  jhq0113@163.com
+     */
+    public function _initComponents()
+    {
+        echo __FUNCTION__.'<br/>';
+    }
+
+    /**
+     * @author Jiang Haiqiang
+     * @email  jhq0113@163.com
+     */
+    public function _initTest()
+    {
+        echo __FUNCTION__.'<br/>';
+    }
+}
