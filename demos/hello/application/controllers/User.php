369be794aa420010ed71a54f4b005112b76af89e
qiang <jhq0113@163.com>
2019-01-02 22:05:22 +0800

    环境安装

--- demos/hello/application/controllers/User.php
+++ demos/hello/application/controllers/User.php
@@ -8,9 +8,24 @@
 
 class UserController extends \Yaf\Controller_Abstract
 {
+    /**
+     * @author Jiang Haiqiang
+     * @email  jhq0113@163.com
+     */
     public function loginAction()
     {
-        $params = $this->getRequest()->getParams();
-        var_dump($params);
+        if($this->_request->isPost()) {
+            $userName = $this->getRequest()->getParam('userName','');
+
+            if($userName == 'jhq0113') {
+                return $this->forward('index','index',[
+                    'userName' => $userName
+                ]);
+            }
+        }
+
+        $this->render('form',[
+            'userName' => $userName
+        ]);
     }
 }
