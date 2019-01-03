9b2ed48d94db09df5e426b0bb5995e0078e3f6e6
qiang <jhq0113@163.com>
2019-01-03 21:50:05 +0800

    环境安装

---
+++ demos/hello/application/controllers/User.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jiang Haiqiang
+ * Date: 2019/1/3
+ * Time: 9:49 PM
+ */
+
+/**
+ * Class User
+ * User Jiang Haiqiang
+ */
+class User extends \Yaf\Controller_Abstract
+{
+    /**
+     * @author Jiang Haiqiang
+     * @email  jhq0113@163.com
+     */
+    public function infoAction()
+    {
+        var_dump($this->_module);
+        die;
+    }
+}
