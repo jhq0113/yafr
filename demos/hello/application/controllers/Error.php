615304f7d6ae98f31af6be9a16d9e7ad19b5b89e
qiang <jhq0113@163.com>
2019-01-05 20:10:46 +0800

    环境安装

---
+++ demos/hello/application/controllers/Error.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jiang Haiqiang
+ * Date: 2019/1/5
+ * Time: 8:09 PM
+ */
+
+/**
+ * Class ErrorController
+ * User Jiang Haiqiang
+ */
+class ErrorController extends \Yaf\Controller_Abstract
+{
+    /**
+     * @author Jiang Haiqiang
+     * @email  jhq0113@163.com
+     */
+    public function init()
+    {
+         \Yaf\Dispatcher::getInstance()->disableView();
+    }
+
+    /**
+     * @param $exception
+     * @author Jiang Haiqiang
+     * @email  jhq0113@163.com
+     */
+    public function errorAction($exception)
+    {
+        /**
+         * @var \Exception $exception
+         */
+
+        var_dump($exception->getMessage());
+    }
+}
