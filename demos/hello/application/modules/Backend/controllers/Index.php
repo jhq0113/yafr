54e050eeb55d9f42195a65427888c35be43c211d
qiang <jhq0113@163.com>
2019-01-05 18:02:18 +0800

    环境安装

--- demos/hello/application/modules/Backend/controllers/Index.php
+++ demos/hello/application/modules/Backend/controllers/Index.php
@@ -12,6 +12,13 @@
  */
 class IndexController extends \extend\ControllerLayout
 {
+    /**
+     * @var string
+     * @author Jiang Haiqiang
+     * @email  jhq0113@163.com
+     */
+    protected $_layout = APPLICATION_PATH.'/application/views/layouts/main.phtml';
+
     public function indexAction()
     {
 
