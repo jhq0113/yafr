d2e0ac8f22f413904255252bba95247f5229ca58
qiang <jhq0113@163.com>
2019-01-05 17:31:00 +0800

    环境安装

---
+++ demos/hello/application/modules/backend/controllers/Index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jiang Haiqiang
+ * Date: 2019/1/5
+ * Time: 5:28 PM
+ */
+
+/**
+ * Class Index
+ * User Jiang Haiqiang
+ */
+class Index extends \Yaf\Controller_Abstract
+{
+    public function indexAction()
+    {
+
+    }
+}
