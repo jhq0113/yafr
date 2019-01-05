aba3a2f1698ed670d5881b482afbf6cdf9b5e817
qiang <jhq0113@163.com>
2019-01-05 19:47:11 +0800

    环境安装

--- demos/hello/application/modules/Rest/controllers/Rest.php
+++ demos/hello/application/modules/Rest/controllers/Rest.php
@@ -12,6 +12,13 @@
  */
 class RestController extends \extend\ControllerLayout
 {
+    /**
+     * @var string
+     * @author Jiang Haiqiang
+     * @email  jhq0113@163.com
+     */
+    protected $_layout = APPLICATION_PATH.'/application/views/main.phtml';
+
     public function indexAction()
     {
 
