06fbce4e4d17dede78dcb08569084cd7cf1b32d3
qiang <jhq0113@163.com>
2019-01-05 21:36:10 +0800

    环境安装

--- demos/hello/application/Bootstrap.php
+++ demos/hello/application/Bootstrap.php
@@ -14,19 +14,28 @@ class Bootstrap extends \Yaf\Bootstrap_Abstract
 
         //$route = new \Yaf\Route\Simple('m','c','a');
         //$route = new \Yaf\Route\Supervar('r');
-        $route = new \Yaf\Route\Regex(
-            '#regex/(\d+)/(\d+)#',
-                [
-                    'module'      => 'rest',
-                    'controller'  => 'rest',
-                    'action'      => 'route',
-                ],
-                [
-                    1 => 'param1',
-                    2 => 'param2',
-                ]
-            );
+        /*$route = new \Yaf\Route\Regex(
+        '#regex/(\d+)/(\d+)#',
+            [
+                'module'      => 'rest',
+                'controller'  => 'rest',
+                'action'      => 'route',
+            ],
+            [
+                1 => 'param1',
+                2 => 'param2',
+            ]
+        );*/
 
-        $router->addRoute('simple',$route);
+        $route = new \Yaf\Route\Rewrite(
+            'rewrite/:param1/:param2',
+            [
+                'module'      => 'rest',
+                'controller'  => 'rest',
+                'action'      => 'route',
+            ]
+        );
+
+        $router->addRoute('newRoute',$route);
     }
 }
