739a7aae29c08bf3758789498da1078cbd52405e
qiang <jhq0113@163.com>
2019-01-05 19:42:33 +0800

    环境安装

---
+++ demos/hello/application/modules/Rest/controllers/Rest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jiang Haiqiang
+ * Date: 2019/1/5
+ * Time: 7:41 PM
+ */
+
+/**
+ * Class RestController
+ * User Jiang Haiqiang
+ */
+class RestController extends \Yaf\Controller_Abstract
+{
+    public function indexAction()
+    {
+
+    }
+}
