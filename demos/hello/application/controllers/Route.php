e072c064b8014b8c0f98075653b7ab19e29d6382
qiang <jhq0113@163.com>
2019-01-05 20:35:51 +0800

    环境安装

---
+++ demos/hello/application/controllers/Route.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jiang Haiqiang
+ * Date: 2019/1/5
+ * Time: 8:34 PM
+ */
+
+/**
+ * Class RouteController
+ * User Jiang Haiqiang
+ */
+class RouteController extends \Yaf\Controller_Abstract
+{
+    public function indexAction()
+    {
+        $query = json_encode($this->_request->getQuery(),JSON_UNESCAPED_UNICODE);
+        exit($query);
+    }
+}
