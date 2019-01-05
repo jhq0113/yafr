cc0373b860ecc971ed0069fbaccdb8e4257a36c1
qiang <jhq0113@163.com>
2019-01-05 23:00:58 +0800

    Simple route

--- demos/hello/application/modules/Rest/controllers/Route.php
+++ demos/hello/application/modules/Rest/controllers/Route.php
@@ -18,8 +18,8 @@ class RouteController extends \Yaf\Controller_Abstract
      */
     public function indexAction()
     {
-        $params = $this->_request->getParams();
-
+        $params['params'] = $this->_request->getParams();
+        $params['query']  = $this->_request->getQuery();
         //$this->_request->getParam()
         exit(json_encode($params,JSON_UNESCAPED_UNICODE));
     }
