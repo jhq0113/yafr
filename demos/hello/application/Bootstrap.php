d4efccd7d4576de7eaa60b5fcce3697444bf5fb9
qiang <jhq0113@163.com>
2019-01-13 13:57:09 +0800

    日志

--- demos/hello/application/Bootstrap.php
+++ demos/hello/application/Bootstrap.php
@@ -31,6 +31,7 @@ class Bootstrap extends \Yaf\Bootstrap_Abstract
         $logConfig = \Yaf\Application::app()->getConfig()->toArray()['log'];
         $logConfig['fileName'] = $logConfig['logPath'].'/'.date('Y-m').'/'.date('d').'/yaf.log';
         unset($logConfig['logPath']);
+        var_dump($logConfig);die;
         \extend\Di::set('log',$logConfig);
     }
 
